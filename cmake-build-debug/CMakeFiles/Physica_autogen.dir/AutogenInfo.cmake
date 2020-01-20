3a361b738eebaf14d96083cbe208894c99cc7709
sigma <NewSigma@163.com>
2020-01-20 19:22:36 +0800

    Create project

---
+++ cmake-build-debug/CMakeFiles/Physica_autogen.dir/AutogenInfo.cmake
@@ -0,0 +1,39 @@
+# Meta
+set(AM_MULTI_CONFIG "SINGLE")
+# Directories and files
+set(AM_CMAKE_BINARY_DIR "/home/sigma/Documents/Coding/Physica/cmake-build-debug/")
+set(AM_CMAKE_SOURCE_DIR "/home/sigma/Documents/Coding/Physica/")
+set(AM_CMAKE_CURRENT_SOURCE_DIR "/home/sigma/Documents/Coding/Physica/")
+set(AM_CMAKE_CURRENT_BINARY_DIR "/home/sigma/Documents/Coding/Physica/cmake-build-debug/")
+set(AM_CMAKE_INCLUDE_DIRECTORIES_PROJECT_BEFORE "")
+set(AM_BUILD_DIR "/home/sigma/Documents/Coding/Physica/cmake-build-debug/Physica_autogen")
+set(AM_SOURCES "/home/sigma/Documents/Coding/Physica/Core/Const.cpp;/home/sigma/Documents/Coding/Physica/Core/Cube2D.cpp;/home/sigma/Documents/Coding/Physica/Core/FEM/Element.cpp;/home/sigma/Documents/Coding/Physica/Core/FEM/Geography.cpp;/home/sigma/Documents/Coding/Physica/Core/Math/BasicAlgebra/BasicCalculates.cpp;/home/sigma/Documents/Coding/Physica/Core/Math/BasicAlgebra/Polynomial.cpp;/home/sigma/Documents/Coding/Physica/Core/Math/BasicAlgebra/RealNumber.cpp;/home/sigma/Documents/Coding/Physica/Core/Math/BasicAlgebra/Solve.cpp;/home/sigma/Documents/Coding/Physica/Core/Math/Optimization/ClimbMountainAlgorithm.cpp;/home/sigma/Documents/Coding/Physica/Core/Math/Optimization/GeneAlgorithm.cpp;/home/sigma/Documents/Coding/Physica/Gui/MainWindow.cpp;/home/sigma/Documents/Coding/Physica/Gui/OpenGL/CubePlayer.cpp;/home/sigma/Documents/Coding/Physica/Gui/OpenGL/Plotter.cpp;/home/sigma/Documents/Coding/Physica/Physica.cpp;/home/sigma/Documents/Coding/Physica/Utils/ArrayWriter.cpp")
+set(AM_HEADERS "/home/sigma/Documents/Coding/Physica/Core/Header/BasicCalculates.h;/home/sigma/Documents/Coding/Physica/Core/Header/ClimbMountainAlgorithm.h;/home/sigma/Documents/Coding/Physica/Core/Header/Const.h;/home/sigma/Documents/Coding/Physica/Core/Header/Cube2D.h;/home/sigma/Documents/Coding/Physica/Core/Header/Element.h;/home/sigma/Documents/Coding/Physica/Core/Header/GeneAlgorithm.h;/home/sigma/Documents/Coding/Physica/Core/Header/Geography.h;/home/sigma/Documents/Coding/Physica/Core/Header/LinearEquations.h;/home/sigma/Documents/Coding/Physica/Core/Header/Polynomial.h;/home/sigma/Documents/Coding/Physica/Core/Header/RealNumber.h;/home/sigma/Documents/Coding/Physica/Core/Header/Solve.h;/home/sigma/Documents/Coding/Physica/Gui/Header/CubePlayer.h;/home/sigma/Documents/Coding/Physica/Gui/Header/MainWindow.h;/home/sigma/Documents/Coding/Physica/Gui/Header/Plotter.h;/home/sigma/Documents/Coding/Physica/Utils/Header/ArrayWriter.h")
+# Qt environment
+set(AM_QT_VERSION_MAJOR "5")
+set(AM_QT_VERSION_MINOR "13")
+set(AM_QT_MOC_EXECUTABLE "/home/sigma/Qt5.13.2/5.13.2/gcc_64/bin/moc")
+set(AM_QT_UIC_EXECUTABLE "/home/sigma/Qt5.13.2/5.13.2/gcc_64/bin/uic")
+set(AM_QT_RCC_EXECUTABLE "/home/sigma/Qt5.13.2/5.13.2/gcc_64/bin/rcc")
+# MOC settings
+set(AM_MOC_SKIP "/home/sigma/Documents/Coding/Physica/cmake-build-debug/Physica_autogen/mocs_compilation.cpp")
+set(AM_MOC_DEFINITIONS "QT_CORE_LIB;QT_GUI_LIB;QT_WIDGETS_LIB")
+set(AM_MOC_INCLUDES "/home/sigma/Documents/Coding/Physica/cmake-build-debug/Physica_autogen/include;/usr/local/cuda-10.1/targets/x86_64-linux/include;/home/sigma/Qt5.13.2/5.13.2/gcc_64/include;/home/sigma/Qt5.13.2/5.13.2/gcc_64/include/QtCore;/home/sigma/Qt5.13.2/5.13.2/gcc_64/./mkspecs/linux-g++;/home/sigma/Qt5.13.2/5.13.2/gcc_64/include/QtGui;/home/sigma/Qt5.13.2/5.13.2/gcc_64/include/QtWidgets;/usr/local/include/opencv4;/usr/include")
+set(AM_MOC_OPTIONS "")
+set(AM_MOC_RELAXED_MODE "FALSE")
+set(AM_MOC_MACRO_NAMES "Q_OBJECT;Q_GADGET;Q_NAMESPACE")
+set(AM_MOC_DEPEND_FILTERS "")
+set(AM_MOC_PREDEFS_CMD "/usr/bin/c++;-dM;-E;-c;/usr/share/cmake-3.10/Modules/CMakeCXXCompilerABI.cpp")
+# UIC settings
+set(AM_UIC_SKIP "/home/sigma/Documents/Coding/Physica/cmake-build-debug/Physica_autogen/mocs_compilation.cpp")
+set(AM_UIC_TARGET_OPTIONS "")
+set(AM_UIC_OPTIONS_FILES "")
+set(AM_UIC_OPTIONS_OPTIONS "")
+set(AM_UIC_SEARCH_PATHS "")
+# RCC settings
+set(AM_RCC_SOURCES "")
+set(AM_RCC_BUILDS "")
+set(AM_RCC_OPTIONS "")
+set(AM_RCC_INPUTS "")
+# Configurations options
+set(AM_CONFIG_SUFFIX_Debug "_Debug")
