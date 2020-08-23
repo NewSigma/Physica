634c2df262dec6f5e99c55d18f89b807dadef0ae
sigma <NewSigma@163.com>
2020-08-23 09:56:57 +0800

    Changed copyright notice and license notice.

--- include/Physica/Core/MultiPrecition/ComplexScalarImpl/FunctionImpl/CElementaryImpl.h
+++ include/Physica/Core/MultiPrecition/ComplexScalarImpl/FunctionImpl/CElementaryImpl.h
@@ -1,5 +1,20 @@
 /*
- * Copyright (c) 2020 NewSigma@163.com. All rights reserved.
+ * Copyright 2020 WeiBo He.
+ *
+ * This file is part of Physica.
+
+ * Physica is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Physica is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Physica.  If not, see <https://www.gnu.org/licenses/>.
  */
 #ifndef PHYSICA_CELEMENTARYIMPL_H
 #define PHYSICA_CELEMENTARYIMPL_H
