c2a4dbff32e383037cc1f3afccf698e48bca438b
sigma <NewSigma@163.com>
2021-06-12 10:40:37 +0800

    Add relationship of r-lamba and r-volume

--- examples/WaterDrop/WaterDrop.cpp
+++ examples/WaterDrop/WaterDrop.cpp
@@ -46,12 +46,12 @@ private:
 public:
     WaterDropSolver(const WaterDropArgs& drop, T rmin, T stepSize_);
     /* Operations */
-    void solve();
-    T findLambda();
+    T solve();
     int output();
     /* Getters */
-    T getMinTangent();
-    T getLambda() { return const3 / const2; }
+    T getVolume() const;
+    T getMinTangent() const;
+    T getLambda() const { return const3 / const2; }
 private:
     void setLambda(const T& lambda) { const3 = const2 * lambda; }
 };
@@ -63,15 +63,20 @@ WaterDropSolver::WaterDropSolver(const WaterDropArgs& drop, T rmin, T stepSize_)
         , const2(drop.rho / drop.sigma)
         , const3(0) {}
 
-void WaterDropSolver::solve() {
-    solver.rungeKutta4([&](const T& r, const Vector<T>& v) {
-            const T momentum = v[1];
-            const T momentum_2_1 = momentum * momentum + T(1);
-            const T sqrt_momentum_2_1 = sqrt(momentum_2_1);
-            const T term1 = (momentum_2_1 * sqrt_momentum_2_1 * (const1 * v[0] + const3));
-            const T term2 = momentum_2_1 * momentum / r;
-            return Vector<T>{momentum, term1 - term2};
-        });
+T WaterDropSolver::solve() {
+    return bisection([&](const T& lambda) { //We use bisection method by experience and without any prove
+               setLambda(lambda);
+               solver.reset();
+               solver.rungeKutta4([&](const T& r, const Vector<T>& v) {
+                       const T momentum = v[1];
+                       const T momentum_2_1 = momentum * momentum + T(1);
+                       const T sqrt_momentum_2_1 = sqrt(momentum_2_1);
+                       const T term1 = (momentum_2_1 * sqrt_momentum_2_1 * (const1 * v[0] + const3));
+                       const T term2 = momentum_2_1 * momentum / r;
+                       return Vector<T>{momentum, term1 - term2};
+                   });
+               return getMinTangent();
+           }, T(0), T(-10), T(10)); //10 is selected by experience
 }
 
 int WaterDropSolver::output() {
@@ -96,32 +101,57 @@ int WaterDropSolver::output() {
     return QApplication::exec();
 }
 
-T WaterDropSolver::getMinTangent() {
+T WaterDropSolver::getVolume() const {
+    const auto& r = solver.getX();
+    const auto& solution = solver.getSolution();
+
+    const size_t length = solution.getColumn();
+    T volumeHelper(0);
+    for (size_t i = 0; i < length; ++i)
+        volumeHelper += solution(0, i) * r[i];
+    return abs(volumeHelper * stepSize);
+}
+
+T WaterDropSolver::getMinTangent() const {
     const auto& solution = solver.getSolution();
     return solution(1, solution.getColumn() - 1);
 }
-
-T WaterDropSolver::findLambda() {
-    return bisection([&](const T& lambda) { //We use bisection method by experience and without any prove
-               setLambda(lambda);
-               solve();
-               solver.reset();
-               return getMinTangent();
-           }, T(0), T(-10), T(10)); //10 is selected by experience
-}
-
+/**
+ * This program calculates the surface equation of a water drop on a infinite smooth plain.
+ * Specifying any two of mass, radius or contact angle should give the other value.
+ */
 int main(int argc, char** argv) {
-    const T radius = 0.005;
+    QApplication app(argc, argv);
+
     const T rmin = 0.000001;
     const T stepSize = rmin;
     const T sigma = 0.074;
     const T rho = 1000;
     const T g = 9.8;
+    T radius = 0.0001;
 
-    QApplication app(argc, argv);
+    const size_t length = 50;
+    Vector<T> r_arr{};
+    r_arr.resize(length);
+    Vector<T> lambda_arr{};
+    lambda_arr.resize(length);
+    Vector<T> volume_arr{};
+    volume_arr.resize(length);
+    for (size_t i = 0; i < length; ++i) {
+        r_arr[i] = radius;
+        WaterDropSolver solver({radius, sigma, rho, 1, 9.8}, rmin, stepSize);
+        const T lambda = solver.solve();
+        lambda_arr[i] = lambda;
+        volume_arr[i] = solver.getVolume();
+        radius += T(0.0001);
+    }
+    Plot* r_lambda = new Plot(r_arr, lambda_arr);
+    r_lambda->show();
+    Plot* r_volume = new Plot(r_arr, volume_arr);
+    r_volume->show();
+
     WaterDropSolver solver({radius, sigma, rho, 1, 9.8}, rmin, stepSize);
-    const T lambda = solver.findLambda();
+    const T lambda = solver.solve();
     std::cout << "Lambda is " << lambda << std::endl;
-    solver.solve();
     return solver.output();
 }
