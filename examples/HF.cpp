578645d4820460ad93adecd4135647e29cbdf5bc
sigma <NewSigma@163.com>
2021-09-17 19:53:34 +0800

    Optimize base set using GA

--- examples/HF.cpp
+++ examples/HF.cpp
@@ -16,6 +16,7 @@
  * You should have received a copy of the GNU General Public License
  * along with Physica.  If not, see <https://www.gnu.org/licenses/>.
  */
+#include "Physica/Core/Math/Optimization/GeneAlgorithm.h"
 #include "Physica/Core/Physics/ElectronStructure/HF/HFSolver.h"
 #include "Physica/Core/Physics/ElectronStructure/HF/GTOnG.h"
 
@@ -24,58 +25,37 @@ using namespace Physica::Core::Physics;
 
 using ScalarType = Scalar<Double, false>;
 
+ScalarType scf_loop_He(const Vector<ScalarType, 4>& alphas) {
+    Molecular<ScalarType> He = Molecular<ScalarType>(1);
+    auto& atoms = He.getAtoms();
+    const Vector<ScalarType> pos_He{0, 0, 0};
+    atoms[0] = pos_He;
+    auto& atomicNumbers = He.getAtomicNumbers();
+    atomicNumbers[0] = 2;
+
+    HFSolver<GaussBase<ScalarType>> solver = HFSolver<GaussBase<ScalarType>>(He, 2, 4);
+    auto& baseSet = solver.getBaseSet();
+    size_t i = 0;
+    baseSet[i++] = GaussBase<ScalarType>(pos_He, abs(alphas[0]), 0, 0, 0);
+    baseSet[i++] = GaussBase<ScalarType>(pos_He, abs(alphas[1]), 0, 0, 0);
+    baseSet[i++] = GaussBase<ScalarType>(pos_He, abs(alphas[2]), 0, 0, 0);
+    baseSet[i++] = GaussBase<ScalarType>(pos_He, abs(alphas[3]), 0, 0, 0);
+    if (!solver.compute(1E-5, 128))
+        return ScalarType(1E5);
+    return solver.getSelfConsistentEnergy();
+}
+
 int main() {
-    {
-        Molecular<ScalarType> He = Molecular<ScalarType>(1);
-        auto& atoms = He.getAtoms();
-        const Vector<ScalarType> pos_He{0, 0, 0};
-        atoms[0] = pos_He;
-        auto& atomicNumbers = He.getAtomicNumbers();
-        atomicNumbers[0] = 2;
-
-        HFSolver<GaussBase<ScalarType>> solver = HFSolver<GaussBase<ScalarType>>(He, 2, 4);
-        auto& baseSet = solver.getBaseSet();
-        size_t i = 0;
-        baseSet[i++] = GaussBase<ScalarType>(pos_He, 0.298073, 0, 0, 0);
-        baseSet[i++] = GaussBase<ScalarType>(pos_He, 1.242567, 0, 0, 0);
-        baseSet[i++] = GaussBase<ScalarType>(pos_He, 5.782948, 0, 0, 0);
-        baseSet[i++] = GaussBase<ScalarType>(pos_He, 38.474970, 0, 0, 0);
-        if (!solver.compute(1E-5, 20)) {
-            std::cout << "[Warning]: Not converged\n";
-            return 1;
-        }
-        std::cout << "Self consistent energy(in hartree): " << solver.getSelfConsistentEnergy() << '\n';
-        std::cout << "Totle energy(in hartree): " << solver.getTotalEnergy() << '\n';
-    }
-    {
-        Molecular<ScalarType> LiF = Molecular<ScalarType>(2);
-        auto& atoms = LiF.getAtoms();
-        const Vector<ScalarType> pos_Li{0, 0, 0};
-        const Vector<ScalarType> pos_F{0, 0, 1};
-        atoms[0] = pos_Li;
-        atoms[1] = pos_F;
-        auto& atomicNumbers = LiF.getAtomicNumbers();
-        atomicNumbers[0] = 3;
-        atomicNumbers[1] = 9;
-
-        HFSolver<GTO3G<ScalarType>> solver = HFSolver<GTO3G<ScalarType>>(LiF, 12, 12);
-        auto& baseSet = solver.getBaseSet();
-        size_t i = 0;
-        for (; i < 3; ++i)
-            baseSet[i] = GTO3G<ScalarType>::randomBase(pos_Li);
-        for (; i < 7; ++i)
-            baseSet[i] = GTO3G<ScalarType>::randomBase(pos_F);
-        baseSet[i++] = GTO3G<ScalarType>::randomBase(pos_F, 1, 0, 0);
-        baseSet[i++] = GTO3G<ScalarType>::randomBase(pos_F, 1, 0, 0);
-        baseSet[i++] = GTO3G<ScalarType>::randomBase(pos_F, 0, 1, 0);
-        baseSet[i++] = GTO3G<ScalarType>::randomBase(pos_F, 0, 1, 0);
-        baseSet[i++] = GTO3G<ScalarType>::randomBase(pos_F, 0, 0, 1);
-        if (!solver.compute(1E-5, 50000)) {
-            std::cout << "[Warning]: Not converged\n";
-            return 1;
-        }
-        std::cout << "Self consistent energy(in hartree): " << solver.getSelfConsistentEnergy() << '\n';
-        std::cout << "Totle energy(in hartree): " << solver.getTotalEnergy() << '\n';
-    }
+    srand(time(nullptr));
+    Vector<ScalarType, 4> x{randomScalar<ScalarType>(ScalarType(0), ScalarType(10)),
+                            randomScalar<ScalarType>(ScalarType(0), ScalarType(10)),
+                            randomScalar<ScalarType>(ScalarType(0), ScalarType(10)),
+                            randomScalar<ScalarType>(ScalarType(0), ScalarType(10))};
+    const Vector<ScalarType, 4> lower{0, 0, 0, 0};
+    const Vector<ScalarType, 4> upper{50, 50, 50, 50};
+    GeneAlgorithm<ScalarType (*)(const Vector<ScalarType, 4>&), Vector<ScalarType, 4>> ga(scf_loop_He, {0.6, 0.1, 100, 50, lower, upper});
+    ga.solve();
+    std::cout << ga.getOptimizedValue() << '\n';
+    std::cout << ga.getOptimizedParams() << '\n';
     return 0;
 }
