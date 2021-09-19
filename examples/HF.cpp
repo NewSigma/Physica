1c9ee70ade9246a82717ba808e85f7284a86d7b4
sigma <NewSigma@163.com>
2021-09-19 21:26:59 +0800

    Use GA to determine base set

--- examples/HF.cpp
+++ examples/HF.cpp
@@ -26,7 +26,7 @@ using namespace Physica::Core::Physics;
 
 using ScalarType = Scalar<Double, false>;
 
-ScalarType scf_loop_He(const Vector<ScalarType, 4>& alphas) {
+ScalarType scf_loop_He(const Vector<ScalarType, 6>& alphas) {
     Molecular<ScalarType> H2 = Molecular<ScalarType>(1);
     auto& atoms = H2.getAtoms();
     const Vector<ScalarType, 3> pos_H1{0, 0, 0};
@@ -36,19 +36,27 @@ ScalarType scf_loop_He(const Vector<ScalarType, 4>& alphas) {
 
     ElectronConfig config = ElectronConfig(1);
     config.setOrbitState(0, ElectronConfig::SingleOccupacy);
-    HFSolver<GaussBase<ScalarType>> solver = HFSolver<GaussBase<ScalarType>>(H2, config, 4);
+    HFSolver<GaussBase<ScalarType>> solver = HFSolver<GaussBase<ScalarType>>(H2, config, 6);
     auto& baseSet = solver.getBaseSet();
     size_t i = 0;
     baseSet[i++] = GaussBase<ScalarType>(pos_H1, abs(alphas[0]), 0, 0, 0);
     baseSet[i++] = GaussBase<ScalarType>(pos_H1, abs(alphas[1]), 0, 0, 0);
     baseSet[i++] = GaussBase<ScalarType>(pos_H1, abs(alphas[2]), 0, 0, 0);
     baseSet[i++] = GaussBase<ScalarType>(pos_H1, abs(alphas[3]), 0, 0, 0);
+    baseSet[i++] = GaussBase<ScalarType>(pos_H1, abs(alphas[4]), 0, 0, 0);
+    baseSet[i++] = GaussBase<ScalarType>(pos_H1, abs(alphas[5]), 0, 0, 0);
     if (!solver.compute(1E-5, 128))
         return ScalarType(1E5);
     return solver.getSelfConsistentEnergy();
 }
 
 int main() {
-    scf_loop_He({13.00773, 1.962079, 0.444529, 0.1219492});
+    srand(time(nullptr));
+    const Vector<ScalarType, 6> lower{0, 0, 0, 0, 0, 0};
+    const Vector<ScalarType, 6> upper{100, 100, 100, 100, 100, 100};
+    GeneAlgorithm<ScalarType (*)(const Vector<ScalarType, 6>&), Vector<ScalarType, 6>> ga(scf_loop_He, {0.6, 0.1, 1000, 500, lower, upper});
+    ga.solve();
+    std::cout << ga.getOptimizedValue() << '\n';
+    std::cout << ga.getOptimizedParams() << '\n';
     return 0;
 }
