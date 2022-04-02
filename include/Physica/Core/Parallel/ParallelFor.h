3165f9a6aeb23c28610de6a986f6685b0860c699
sigma <NewSigma@163.com>
2022-04-02 21:43:51 +0800

    Refactor parallel_for

--- include/Physica/Core/Parallel/ParallelFor.h
+++ include/Physica/Core/Parallel/ParallelFor.h
@@ -25,22 +25,22 @@
 
 namespace Physica::Core::Parallel {
     template<class Functor>
-    std::vector<std::future<void>> parallel_for(Functor func, size_t loopCount, size_t core) {
+    std::vector<std::future<void>> parallel_for(Functor func, unsigned int loopCount, unsigned int core) {
         assert(loopCount >= core);
         assert(core > 0);
-        const size_t maxLoopPerCore = (loopCount + core - 1) / core;
-        size_t from = 0; 
-        size_t to = maxLoopPerCore;
+        const unsigned int maxLoopPerCore = (loopCount + core - 1) / core;
+        unsigned int from = 0; 
+        unsigned int to = maxLoopPerCore;
         std::vector<std::future<void>> result{};
         result.reserve(core);
-        for (size_t i = 0; i < core; ++i) {
+        for (unsigned int i = 0; i < core; ++i) {
             result.push_back(ThreadPool::getInstance().schedule(
-                [=](size_t from, size_t to) -> void {
-                    for (size_t i = from; i < to; ++i)
+                [=](unsigned int from_, unsigned int to_) -> void {
+                    for (unsigned int i = from_; i < to_; ++i)
                         func(i);
                 }, from, to));
             from += maxLoopPerCore;
-            const size_t next_to = to + maxLoopPerCore;
+            const unsigned int next_to = to + maxLoopPerCore;
             to = next_to > loopCount ? loopCount : next_to;
         }
         return result;
