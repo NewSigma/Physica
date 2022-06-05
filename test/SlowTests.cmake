031c28fff7548ac7da698bb39fb44e3724c156b4
sigma <NewSigma@163.com>
2022-06-05 11:40:04 +0800

    Enable slow tests in release mode only

---
+++ test/SlowTests.cmake
@@ -0,0 +1,5 @@
+## Optimization
+physica_add_test(Adam Core/Math/Optimization/AdamTest.cpp)
+# Physics
+physica_add_test(DimEstimator Core/Physics/DimEstimator.cpp)
+physica_add_test(KSSolver Core/Physics/KSSolver.cpp)
