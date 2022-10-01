8c314071c9f90ccc1a795b533f311fa0d3bb55e9
NewSigma <NewSigma@163.com>
2022-10-01 16:32:56 +0800

    Add Vector_cuda test

---
+++ test/Core/Math/Algebra/LinearAlgebra/Vector/Vector.cu
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2022 WeiBo He.
+ *
+ * This file is part of Physica.
+ *
+ * Physica is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Physica is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Physica.  If not, see <https://www.gnu.org/licenses/>.
+ */
+#include "Physica/Core/Math/Algebra/LinearAlgebra/Vector/Vector.h"
+
+using namespace Physica::Core;
+using ScalarType = Scalar<Float, false>;
+using VectorType = Vector<ScalarType>;
+
+int main() {
+    VectorType a{1, 2, 3, 4};
+    auto d_a = a.toDevice();
+    device_obj<VectorType> d_b(4);
+    d_b = d_a;
+    if (cudaStreamSynchronize(nullptr) != cudaError_t::cudaSuccess)
+        return 1;
+    VectorType b = d_b.toHost();
+    if (a != b)
+        return 1;
+    return 0;
+}
---
+++ test/CudaTests.cmake
@@ -0,0 +1,5 @@
+# Core
+## Math
+### Algebra
+#### Linear Algebra
+physica_add_test(Vector_cuda Core/Math/Algebra/LinearAlgebra/Vector/Vector.cu)
