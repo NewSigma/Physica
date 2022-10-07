b3d8edb61984460abf0b2aa0a4245cb8300d4985
NewSigma <NewSigma@163.com>
2022-10-07 15:50:24 +0800

    Add SavitzkyGolay filter

---
+++ include/Physica/Core/Physics/Filter/SavitzkyGolay.h
@@ -0,0 +1,100 @@
+/*
+ * Copyright 2022 WeiBo He.
+ *
+ * This file is part of Physica.
+ *
+ * Physica is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Physica is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Physica.  If not, see <https://www.gnu.org/licenses/>.
+ */
+#pragma once
+
+#include "Physica/Core/Math/Algebra/LinearAlgebra/Matrix/DenseMatrix.h"
+
+namespace Physica::Core {
+    /**
+     * Reference:
+     * [1] H.Press, William, A.Teukolsky, Saul, Vetterling, William T., Flannery, Brian P..
+     * C++数值算法[M].北京: Publishing House of Electronics Industry, 2009:480-484
+     */
+    template<class ScalarType>
+    class SavitzkyGolay {
+        using MatrixType = DenseMatrix<ScalarType>;
+        unsigned char lRange;
+        unsigned char rRange;
+        MatrixType coeffs;
+        MatrixType inv;
+    public:
+        SavitzkyGolay(unsigned char lRange_, unsigned char rRange_, size_t order, ScalarType delta);
+        SavitzkyGolay(const SavitzkyGolay&) = default;
+        SavitzkyGolay(SavitzkyGolay&&) noexcept = default;
+        ~SavitzkyGolay() = default;
+        /* Operators */
+        SavitzkyGolay& operator=(SavitzkyGolay filter) noexcept;
+        /* Operations */
+        void swap(SavitzkyGolay& filter) noexcept;
+        template<class VectorType>
+        void smooth(LValueVector<VectorType>& data) const;
+        /* Getters */
+        [[nodiscard]] size_t getOrder() const noexcept { return coeffs.getRow() - 1; }
+        [[nodiscard]] size_t getWindowSize() const noexcept { return coeffs.getRow(); }
+    };
+
+    template<class ScalarType>
+    SavitzkyGolay<ScalarType>::SavitzkyGolay(unsigned char lRange_, unsigned char rRange_, size_t order, ScalarType delta)
+            : lRange(lRange_), rRange(rRange_), coeffs(lRange_ + rRange_ + 1, order + 1), inv(order + 1, order + 1) {
+        for (size_t major = 0; major < coeffs.getMaxMajor(); ++major) {
+            for (size_t minor = 0; minor < coeffs.getMaxMinor(); ++minor) {
+                if (major == 0 && minor == 0)
+                    coeffs.refFromMajorMinor(0, 0) = 1;
+                const size_t row = MatrixOption::rowFromMajorMinor<MatrixType>(major, minor);
+                const size_t column = MatrixOption::columnFromMajorMinor<MatrixType>(major, minor);
+                const int i = int(row) - int(lRange);
+                coeffs.refFromMajorMinor(major, minor) = pow(delta * i, ScalarType(column));
+            }
+        }
+
+        inv = coeffs.transpose() * coeffs;
+        MatrixType temp = inv.inverse();
+        inv.swap(temp);
+    }
+    
+    template<class ScalarType>
+    SavitzkyGolay<ScalarType>& SavitzkyGolay<ScalarType>::operator=(SavitzkyGolay<ScalarType> filter) noexcept {
+        swap(filter);
+        return *this;
+    }
+
+    template<class ScalarType>
+    template<class VectorType>
+    void SavitzkyGolay<ScalarType>::smooth(LValueVector<VectorType>& data) const {
+        const size_t windowSize = getWindowSize();
+        Vector<ScalarType> mask(windowSize);
+        for (size_t i = 0; i < windowSize; ++i)
+            mask[i] = inv.row(0).asVector() * coeffs.row(i).asVector();
+
+        Vector<ScalarType> temp = data;
+        for (size_t pos = lRange; pos < data.getLength() - rRange; ++pos) {
+            auto window = data.getDerived().segment(pos - lRange, pos - lRange + windowSize);
+            temp[pos] = mask * window;
+        }
+        data.getDerived() = std::move(temp);
+    }
+
+    template<class ScalarType>
+    void SavitzkyGolay<ScalarType>::swap(SavitzkyGolay<ScalarType>& filter) noexcept {
+        std::swap(lRange, filter.lRange);
+        std::swap(rRange, filter.rRange);
+        coeffs.swap(filter.coeffs);
+        inv.swap(filter.inv);
+    }
+}
