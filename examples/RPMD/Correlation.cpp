b28fd0c6d8dc7f6dedf4c5fac654116dbf2ac3d8
NewSigma <NewSigma@163.com>
2022-10-22 15:54:16 +0800

    Add examples

---
+++ examples/RPMD/Correlation.cpp
@@ -0,0 +1,152 @@
+#include <iostream>
+#include <fstream>
+#include "Physica/Core/Physics/MD/RPMD.h"
+#include "Physica/Core/Physics/MD/ForceModel/PairModel.h"
+#include "Physica/Core/Parallel/Executor/ThreadExecutor.h"
+#include "Physica/Utils/Random.h"
+
+using namespace Physica::Core;
+using namespace Physica::Core::Parallel;
+using ScalarType = Scalar<Double, false>;
+using PosScalarType = Scalar<Double, false>;
+constexpr size_t numReplica = 48;
+constexpr double temperatureT = PhyConst<AU>::kToTemperature(14);
+constexpr double thermostatTime = PhyConst<AU>::secondToTime(100 * 1E-15);
+constexpr double timeStep = PhyConst<AU>::secondToTime(1E-15) * 0.5;
+constexpr unsigned int numMolecular = 108;
+constexpr double pair_cutoff = 15;
+constexpr double molarVolume = 25.6;
+constexpr double mass = PhyConst<AU>::atomMass(1) * 2;
+
+ScalarType pot_functor(ScalarType r) {
+    constexpr double alpha = 1.713;
+    constexpr double beta = 1.5671;
+    constexpr double gamma = 0.00993;
+    constexpr double cutoff = 8.32;
+    constexpr double c6 = 12.14;
+    constexpr double c8 = 215.2;
+    constexpr double c9 = 143.1;
+    constexpr double c10 = 4813.9;
+
+    const ScalarType r2 = square(r);
+    ScalarType result = exp(-r2 * gamma - r * beta + alpha);
+    const ScalarType rep_r = reciprocal(r);
+    const ScalarType rep_r2 = square(rep_r);
+    const ScalarType rep_r4 = square(rep_r2);
+    const ScalarType rep_r6 = rep_r4 * rep_r2;
+    const ScalarType rep_r8 = square(rep_r4);
+    const ScalarType rep_r9 = rep_r8 * rep_r;
+    const ScalarType rep_r10 = rep_r6 * rep_r4;
+    const ScalarType g = rep_r6 * c6 + rep_r8 * c8 - rep_r9 * c9 + rep_r10 * c10;
+
+    if (r < cutoff) {
+        const ScalarType f_cutoff = exp(-square(rep_r * cutoff - 1));
+        result -= g * f_cutoff;
+    }
+    else
+        result -= g;
+    return result;
+}
+
+ScalarType force(ScalarType r) {
+    constexpr double alpha = 1.713;
+    constexpr double beta = 1.5671;
+    constexpr double gamma = 0.00993;
+    constexpr double cutoff = 8.32;
+    constexpr double c6 = 12.14;
+    constexpr double c8 = 215.2;
+    constexpr double c9 = 143.1;
+    constexpr double c10 = 4813.9;
+
+    const ScalarType r2 = square(r);
+    ScalarType result = exp(-r2 * gamma - r * beta + alpha) * (r * (gamma * 2) + beta);
+    const ScalarType rep_r = reciprocal(r);
+    const ScalarType rep_r2 = square(rep_r);
+    const ScalarType rep_r4 = square(rep_r2);
+    const ScalarType rep_r6 = rep_r4 * rep_r2;
+    const ScalarType rep_r7 = rep_r6 * rep_r;
+    const ScalarType rep_r9 = rep_r7 * rep_r2;
+    const ScalarType rep_r10 = rep_r6 * rep_r4;
+    const ScalarType rep_r11 = rep_r7 * rep_r4;
+
+    const ScalarType d_g = rep_r10 * (9 * c9) - rep_r11 * (10 * c10) - rep_r9 * (8 * c8) - rep_r7 * (6 * c6);
+    if (r < cutoff) {
+        const ScalarType rep_r8 = square(rep_r4);
+        const ScalarType g = rep_r6 * c6 + rep_r8 * c8 - rep_r9 * c9 + rep_r10 * c10;
+        const ScalarType f_cutoff = exp(-square(rep_r * cutoff - 1));
+        result += (d_g + g * rep_r * (rep_r2 * (2 * cutoff * cutoff) - rep_r * (2 * cutoff))) * f_cutoff;
+    }
+    else {
+        result += d_g;
+    }
+    return result;
+}
+
+template<class RandomGenerator>
+RPMD<ScalarType, PosScalarType> makeSystem(RandomGenerator& gen) {
+    using MDCellType = typename RPMD<ScalarType, PosScalarType>::MDCellType;
+    typename MDCellType::LatticeMatrix lattice = MDCellType::LatticeMatrix::unitMatrix(3);
+    typename MDCellType::PositionMatrix pos(numMolecular, 3);
+    std::uniform_real_distribution dist{};
+    for (auto& elem : pos)
+        elem = dist(gen);
+    typename MDCellType::MassVector massVec(numMolecular, mass);
+    MDCellType cell(std::move(lattice), std::move(pos), std::move(massVec));
+
+    const double factor = (std::cbrt(numMolecular * molarVolume / PhyConst<SI>::avogadroNa) / 100) / PhyConst<SI>::bohrRadius;
+    cell.scale(factor);
+
+    return RPMD<ScalarType, PosScalarType>(std::move(cell), numReplica, numReplica, temperatureT, thermostatTime, timeStep);
+}
+/**
+ * Reference:
+ * [1] Miller TF, Manolopoulos DE. 2005. Quantum diffusion in liquid para-hydrogen from ring polymer molecular dynamics. J. Chem. Phys. 122:184503
+ */
+int main() {
+    constexpr size_t CorrStep = 3000;
+    constexpr double factor = 1.0 / (numMolecular * mass * mass) * (PhyConst<AU>::bohrToAngstorm(1) * PhyConst<AU>::bohrToAngstorm(1)) / (PhyConst<AU>::timeToSecond(1) * 1E12 * PhyConst<AU>::timeToSecond(1) * 1E12);
+
+    ThreadPool::initThreadPool(4);
+    ThreadPool& pool = ThreadPool::getInstance();
+    {
+        std::mt19937 gen(1082247429173841685);
+        RPMD<ScalarType, PosScalarType> rpmd = makeSystem(gen);
+        rpmd.initMomentum(gen);
+
+        PairModel<ScalarType, PosScalarType, decltype(&force)> pair(ScalarType(pair_cutoff), force, pot_functor);
+        rpmd.updateForce<decltype(pair), ThreadExecutor>(pair);
+
+        Vector<ScalarType> corr(CorrStep, 0);
+        Vector<ScalarType> corr_var(CorrStep, 0);
+        Vector<ScalarType> temp(CorrStep);
+
+        for (unsigned int i = 0; i < 6; ++i) {
+            temp = ScalarType(0);
+            for (unsigned int j = 0; j < 100; ++j) {
+                rpmd.setThermostatTime(PhyConst<AU>::secondToTime(100 * 1E-15));
+                rpmd.nvt_step_for<decltype(gen), decltype(pair), ThreadExecutor>(PhyConst<AU>::secondToTime(2 * 1E-12), gen, pair);
+                rpmd.setThermostatTime(PhyConst<AU>::secondToTime(100 * 1E15));
+                const auto p0 = rpmd.getMomentum();
+                for (unsigned int k = 0; k < CorrStep; ++k) {
+                    toNextMean(temp[k], j, hadamard(rpmd.getMomentum(), p0).sum() * factor);
+                    rpmd.nvt_step<decltype(gen), decltype(pair), ThreadExecutor>(gen, pair);
+                }
+            }
+
+            for (unsigned int k = 0; k < CorrStep; ++k)
+                toNextVariance(corr_var[k], corr[k], i, temp[k]);
+        }
+
+        {
+            std::ofstream fout("corr");
+            fout << corr;
+        }
+        {
+            std::ofstream fout("corr_var");
+            fout << corr_var;
+        }
+    }
+    pool.shouldExit();
+    ThreadPool::deInitThreadPool();
+    return 0;
+}
---
+++ examples/RPMD/RDF.cpp
@@ -0,0 +1,155 @@
+/*
+ * Copyright 2022 WeiBo He.
+ *
+ * This file is part of Physica.
+ *
+ * Physica is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Physica is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Physica.  If not, see <https://www.gnu.org/licenses/>.
+ */
+#include <iostream>
+#include <fstream>
+#include "Physica/Core/Physics/MD/ForceModel/Q_TIP4P.h"
+#include "Physica/Core/Physics/ElectronicStructure/CrystalCell.h"
+#include "Physica/Core/Physics/MD/RPMD.h"
+#include "Physica/Core/Parallel/Executor/ThreadExecutor.h"
+#include "Physica/Utils/Random.h"
+#include "Physica/Core/Physics/MD/RDF.h"
+#include "Physica/Core/IO/Poscar.h"
+
+using namespace Physica::Core;
+using namespace Physica::Core::Parallel;
+using ScalarType = Scalar<Double, false>;
+using PosScalarType = Scalar<Double, false>;
+using ForceModel = Q_TIP4P<ScalarType, PosScalarType, 4>;
+constexpr size_t numReplica = 32;
+constexpr double temperatureT = PhyConst<AU>::kToTemperature(298);
+constexpr double thermostatTime = PhyConst<AU>::secondToTime(100 * 1E-15);
+constexpr double timeStep = PhyConst<AU>::secondToTime(1E-15) * 0.25;
+constexpr double pair_cutoff = PhyConst<AU>::angstormToBohr(9);
+constexpr double massMoleculeInSI = PhyConst<SI>::atomMass(1) * 2 + PhyConst<SI>::atomMass(8);
+
+template<class RandomGenerator>
+Vector<PosScalarType, 3> randomVector(RandomGenerator& gen) {
+    std::uniform_real_distribution dist{};
+    const PosScalarType theta(dist(gen) * M_PI);
+    const PosScalarType phi(dist(gen) * M_PI * 2);
+    Vector<PosScalarType, 3> result{cos(phi) * sin(theta), sin(phi) * sin(theta), cos(theta)};
+    result *= PosScalarType(ForceModel::equalR);
+    return result;
+}
+
+template<class RandomGenerator>
+MDCell<ScalarType, PosScalarType> makeSystem(unsigned int cellSize, RandomGenerator& gen) {
+    constexpr size_t MoleculePerCell = 4;
+    constexpr size_t maxIndexH = MoleculePerCell * 2;
+    constexpr size_t maxIndexO = MoleculePerCell * 3;
+    constexpr size_t numAtom = MoleculePerCell * 3;
+
+    PosScalarType cellVolume = ((MoleculePerCell * massMoleculeInSI * 1000 / 0.997) * 1E-6) / (PhyConst<SI>::bohrRadius * PhyConst<SI>::bohrRadius * PhyConst<SI>::bohrRadius);
+    const PosScalarType latticeFactor(cbrt(cellVolume));
+    typename CrystalCell::LatticeMatrix lattice = CrystalCell::LatticeMatrix::unitMatrix(3);
+    lattice *= latticeFactor;
+
+    typename CrystalCell::PositionMatrix pos(numAtom, 3);
+    std::uniform_real_distribution dist{};
+    for (size_t i = 0; i < MoleculePerCell; ++i) {
+        auto posO = pos.row(i + maxIndexH);
+        if (i == 0) {
+            posO[0] = latticeFactor * (0.25 + (dist(gen) - 0.5) / 5);
+            posO[1] = latticeFactor * (0.25 + (dist(gen) - 0.5) / 5);
+            posO[2] = latticeFactor * (0.25 + (dist(gen) - 0.5) / 5);
+        }
+        else if (i == 1) {
+            posO[0] = latticeFactor * (0.75 + (dist(gen) - 0.5) / 5);
+            posO[1] = latticeFactor * (0.75 + (dist(gen) - 0.5) / 5);
+            posO[2] = latticeFactor * (0.25 + (dist(gen) - 0.5) / 5);
+        }
+        else if (i == 2) {
+            posO[0] = latticeFactor * (0.75 + (dist(gen) - 0.5) / 5);
+            posO[1] = latticeFactor * (0.25 + (dist(gen) - 0.5) / 5);
+            posO[2] = latticeFactor * (0.75 + (dist(gen) - 0.5) / 5);
+        }
+        else if (i == 3) {
+            posO[0] = latticeFactor * (0.25 + (dist(gen) - 0.5) / 5);
+            posO[1] = latticeFactor * (0.75 + (dist(gen) - 0.5) / 5);
+            posO[2] = latticeFactor * (0.75 + (dist(gen) - 0.5) / 5);
+        }
+        auto posH1 = pos.row(2 * i);
+        auto posH2 = pos.row(2 * i + 1);
+        posH1 = posO + randomVector(gen);
+        posH2 = posO + randomVector(gen);
+    }
+
+    typename CrystalCell::AtomicArray atomicNumbers(numAtom);
+    for (size_t i = 0; i < maxIndexH; ++i)
+        atomicNumbers[i] = 1;
+    for (size_t i = maxIndexH; i < maxIndexO; ++i)
+        atomicNumbers[i] = 8;
+
+    CrystalCell cell(std::move(lattice), std::move(pos), std::move(atomicNumbers), CrystalCell::Type::Cartesian);
+    cell.unitToSuper(cellSize, cellSize, cellSize);
+    return MDCell<ScalarType, PosScalarType>(std::move(cell));
+}
+/**
+ * Reference:
+ * [1] S. Habershon, T. E. Markland, and D. E. Manolopoulosa, J. Chem. Phys. 131, 024501(2009)
+ */
+int main() {
+    std::mt19937 gen{};
+
+    auto cell = makeSystem(2, gen);
+    ForceModel::sortPosition(cell);
+    RPMD<ScalarType, PosScalarType> rpmd(std::move(cell), numReplica, numReplica, temperatureT, thermostatTime, timeStep);
+    rpmd.initMomentum(gen);
+    ForceModel model(rpmd.phaseToCell(0), pair_cutoff);
+
+    RDF<PosScalarType> rdf;
+    {
+        Physica::Utils::Array<bool> isFromParticle(rpmd.getNumParticle());
+        Physica::Utils::Array<bool> isToParticle(rpmd.getNumParticle());
+        for (size_t i = 0; i < isFromParticle.getLength(); ++i) {
+            const bool isHydrogen = i < isFromParticle.getLength() * 2 / 3;
+            isFromParticle[i] = isHydrogen;
+            isToParticle[i] = isHydrogen;
+        }
+        rdf = RDF<PosScalarType>(std::move(isFromParticle), std::move(isToParticle), PhyConst<AU>::angstormToBohr(0.01), 700);
+    }
+
+    ThreadPool::initThreadPool(4);
+    ThreadPool& pool = ThreadPool::getInstance();
+    {
+        rpmd.nvt_step_for<decltype(gen), decltype(model), ThreadExecutor>(PhyConst<AU>::secondToTime(2 * 1E-12), gen, model);
+        for (size_t i = 0; i < 1000; ++i) {
+            rpmd.nvt_step<decltype(gen), decltype(model), ThreadExecutor>(gen, model);
+            rpmd.normalizeCentroid();
+            rdf.sample(rpmd.makeAverageCell());
+        }
+    }
+    pool.shouldExit();
+    ThreadPool::deInitThreadPool();
+    {
+        std::ofstream fout("dists");
+        fout << rdf.makeDists();
+    }
+    {
+        std::ofstream fout("rdf");
+        fout << rdf.makeRDF(rpmd.getVolume());
+    }
+    {
+        Poscar poscar(rpmd.getLattice(), rpmd.makeCentroidPos(), {rpmd.getNumParticle() * 2 / 3, rpmd.getNumParticle() / 3}, Poscar::Type::Cartesian);
+        std::ofstream fout("H2O.vasp");
+        fout << poscar;
+        fout.close();
+    }
+    return 0;
+}
