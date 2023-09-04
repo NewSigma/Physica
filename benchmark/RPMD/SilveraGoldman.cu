93fbf619da9f0fb613152a17629ffbd4e9393e4d
NewSigma <NewSigma@163.com>
2023-09-04 11:18:57 +0800

    Add Benchmark for PairModel_cuda

---
+++ benchmark/RPMD/SilveraGoldman.cu
@@ -0,0 +1,73 @@
+/*
+ * Copyright 2023 WeiBo He.
+ *
+ * This file is part of Physica.
+ *
+ * Physica is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Physica is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Physica.  If not, see <https://www.gnu.org/licenses/>.
+ */
+#include <iostream>
+#include <fstream>
+#include <gperftools/profiler.h>
+#include "Physica/Core/Physics/MD/RPMD.h"
+#include "Physica/Core/Physics/MD/Thermostat/TRPMDThermo.h"
+#include "Physica/Core/Physics/MD/KineticModel/FreeModel.h"
+#include "Physica/Core/Physics/MD/ForceModel/SilveraGoldman.cuh"
+#include "Physica/Core/Parallel/Executor/CudaExecutor.cuh"
+#include "Physica/Core/Math/Random/RandomPool.h"
+
+using namespace Physica::Core;
+using ScalarType = Scalar<Float>;
+using PosScalarType = ScalarType;
+using ForceModel = device_obj<SilveraGoldman<ScalarType, PosScalarType>>;
+using KineticModel = FreeModel<ScalarType, PosScalarType, 3>;
+using RandomGenerator = std::mt19937;
+constexpr size_t numReplica = 24;
+constexpr double temperatureT = PhyConst<AU>::kToTemperature(25);
+constexpr double timeStep = PhyConst<AU>::secondToTime(1E-15) * 0.5;
+constexpr unsigned int numMolecular = 108;
+constexpr double pair_cutoff = 15;
+constexpr double molarVolume = 31.7;
+constexpr double mass = PhyConst<AU>::atomMass(1) * 2;
+
+template<class RandomGenerator>
+RPMD<ScalarType, PosScalarType> makeSystem(RandomGenerator& gen) {
+    using MDCellType = typename RPMD<ScalarType, PosScalarType>::MDCellType;
+    typename MDCellType::LatticeMatrix lattice = MDCellType::LatticeMatrix::unitMatrix(3);
+    typename MDCellType::PositionMatrix pos(numMolecular, 3);
+    std::uniform_real_distribution dist{};
+    for (auto& elem : pos)
+        elem = dist(gen);
+    typename MDCellType::MassVector massVec(numMolecular, mass);
+    MDCellType cell(std::move(lattice), std::move(pos), std::move(massVec));
+
+    const double factor = (std::cbrt(numMolecular * molarVolume / PhyConst<SI>::avogadroNa) / 100) / PhyConst<SI>::bohrRadius;
+    cell.scale(factor);
+
+    return RPMD<ScalarType, PosScalarType>(std::move(cell), numReplica, numReplica, temperatureT, timeStep);
+}
+/**
+ * Reference:
+ * [1] Miller TF, Manolopoulos DE. 2005. Quantum diffusion in liquid para-hydrogen from ring polymer molecular dynamics. J. Chem. Phys. 122:184503
+ */
+int main() {
+    RandomPool<RandomGenerator>::FixedSeed = 10000;
+    auto& gen = RandomPool<RandomGenerator>::getGen();
+    RPMD<ScalarType, PosScalarType> rpmd = makeSystem(gen);
+    rpmd.initMomentum(gen);
+
+    KineticModel kineticModel(temperatureT, numReplica);
+    ForceModel forceModel(numMolecular, pair_cutoff);
+    rpmd.nve_step_for<KineticModel, ForceModel, CudaExecutor>(PhyConst<AU>::secondToTime(1 * 1E-11), kineticModel, forceModel);
+    return 0;
+}
