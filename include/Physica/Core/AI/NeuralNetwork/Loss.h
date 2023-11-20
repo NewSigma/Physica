e68fcdc894eef3a6bb4b6730db8247dee4d1e350
NewSigma <NewSigma@163.com>
2023-11-20 11:30:11 +0800

    Loss: Refactor

--- include/Physica/Core/AI/NeuralNetwork/Loss.h
+++ include/Physica/Core/AI/NeuralNetwork/Loss.h
@@ -26,18 +26,15 @@ namespace Physica::Core {
         using PlainScalar = typename ScalarType::PlainScalar;
         using VectorType = Vector<ScalarType>;
     public:
-        [[nodiscard]] static ScalarType crossEntropy(const VectorType& result, const VectorType& answer);
+        [[nodiscard]] static ScalarType crossEntropy(const VectorType& result, size_t answer);
         [[nodiscard]] static ScalarType polar_rate(ScalarType train_loss, ScalarType valid_loss);
         [[nodiscard]] static ScalarType mixed_loss(ScalarType train_loss, ScalarType valid_loss);
     };
 
     template<class ScalarType>
-    ScalarType Loss<ScalarType>::crossEntropy(const VectorType& result, const VectorType& answer) {
-        for (size_t i = 0; i < answer.getLength(); ++i)
-            if (!answer[i].isZero())
-                return -ln(softmax(result).calc(i) + ScalarType(std::numeric_limits<PlainScalar>::min())); //Add minimum to avoid ln(0)
-        assert(false && "[Error]: Invalid argument");
-        return ScalarType(0);
+    ScalarType Loss<ScalarType>::crossEntropy(const VectorType& result, size_t answer) {
+        assert(answer < result.getLength() && "[Error]: The label is not exist");
+        return -ln(softmax(result).calc(answer) + ScalarType(std::numeric_limits<PlainScalar>::min())); //Add minimum to avoid ln(0)
     }
     /**
      * \returns polarization rate, the lower the better, minus value means overfitting
--- test/Core/AI/Loss.cpp
+++ test/Core/AI/Loss.cpp
@@ -24,13 +24,11 @@ using ScalarType = Scalar<Float>;
 
 void testSelectProperty() {
     const Vector<ScalarType> result{-3.34036088, -109.5531235, 13.51656151, 11.29175949};
-    const Vector<ScalarType> answer1{0, 0, 0, 1};
-    const ScalarType l1 = Loss<ScalarType>::crossEntropy(result, answer1);
+    const ScalarType l1 = Loss<ScalarType>::crossEntropy(result, 3);
     if (!std::isfinite(double(l1)))
         exit(EXIT_FAILURE);
 
-    const Vector<ScalarType> answer2{0, 1, 0, 0};
-    const ScalarType l2 = Loss<ScalarType>::crossEntropy(result, answer2);
+    const ScalarType l2 = Loss<ScalarType>::crossEntropy(result, 1);
     if (!std::isfinite(double(l2)))
         exit(EXIT_FAILURE);
 }
