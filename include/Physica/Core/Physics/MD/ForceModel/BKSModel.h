2b8878a040ed8dc8bb50313e080dfb4873a8bed0
NewSigma <NewSigma@163.com>
2023-12-30 23:17:50 +0800

    BKSModel: Avoid too near

--- include/Physica/Core/Physics/MD/ForceModel/BKSModel.h
+++ include/Physica/Core/Physics/MD/ForceModel/BKSModel.h
@@ -23,11 +23,11 @@
 #include "PairModel.h"
 
 namespace Physica::Core {
-    template<class ScalarType, class EwaldType> class BKSModel;
+    template<class ScalarType, class EwaldType, bool AvoidTooNear> class BKSModel;
 
     namespace Internal {
-        template<class T, class EwaldType>
-        class Traits<BKSModel<T, EwaldType>> {
+        template<class T, class EwaldType, bool AvoidTooNear>
+        class Traits<BKSModel<T, EwaldType, AvoidTooNear>> {
         public:
             using ScalarType = T;
             constexpr static bool IsPeriodBoundary = true;
@@ -42,12 +42,13 @@ namespace Physica::Core {
      * Reference:
      * [1] Phys. Rev. Lett. 64, 1955 (1990); https://doi.org/10.1103/PhysRevLett.64.1955
      */
-    template<class ScalarType, class EwaldType>
-    class BKSModel : public PairModel<BKSModel<ScalarType, EwaldType>> {
-        using This = BKSModel<ScalarType, EwaldType>;
+    template<class ScalarType, class EwaldType, bool AvoidTooNear>
+    class BKSModel : public PairModel<BKSModel<ScalarType, EwaldType, AvoidTooNear>> {
+        using This = BKSModel<ScalarType, EwaldType, AvoidTooNear>;
         using Base = PairModel<This>;
         using AABModelType = AABModel<ScalarType>;
         using REwaldType = typename Internal::Traits<EwaldType>::REwaldType;
+        using DoublePotType = typename std::conditional<AvoidTooNear, ScalarType, PlainStruct<void>>::type;
     public:
         using typename Base::PlainScalar;
         using typename Base::MDCellType;
@@ -62,14 +63,18 @@ namespace Physica::Core {
         constexpr static double A_OO = PhyConst<AU>::eVToHartree(1388.773);
         constexpr static double b_OO = 2.76 * PhyConst<AU>::bohrToAngstorm(1);
         constexpr static double c_OO = PhyConst<AU>::eVToHartree(175) * angstormInBohr6;
+        constexpr static double r0_OO = 2.8414313142730038;
         constexpr static double chargeO = -1.2;
 
         constexpr static double A_SiO = PhyConst<AU>::eVToHartree(18003.7572);
         constexpr static double b_SiO = 4.87318 * PhyConst<AU>::bohrToAngstorm(1);
         constexpr static double c_SiO = PhyConst<AU>::eVToHartree(133.5381) * angstormInBohr6;
+        constexpr static double r0_SiO = 2.075401485810223;
         constexpr static double chargeSi = -2 * chargeO;
     private:
         EwaldType ewald;
+        DoublePotType doublePot0_OO;
+        DoublePotType doublePot0_SiO;
     public:
         BKSModel(const MDCellType& refer_cell, PlainScalar cutoff, EwaldType ewald_);
         BKSModel(const BKSModel&) = default;
@@ -101,20 +106,47 @@ namespace Physica::Core {
         /* Static members */
         inline static PermutationMatrix<ScalarType> sortPosition(MDCellType& cell);
     private:
+        [[nodiscard]] inline static ScalarType pot_functor_impl(ScalarType A, ScalarType b, ScalarType c, ScalarType r, ScalarType r2);
         [[nodiscard]] inline static bool isCellOrdered(const MDCellType& cell);
     };
 
-    template<class ScalarType, class EwaldType>
-    BKSModel<ScalarType, EwaldType>::BKSModel(const MDCellType& refer_cell, PlainScalar cutoff, EwaldType ewald_)
+    template<class ScalarType, class EwaldType, bool AvoidTooNear>
+    BKSModel<ScalarType, EwaldType, AvoidTooNear>::BKSModel(const MDCellType& refer_cell, PlainScalar cutoff, EwaldType ewald_)
             : Base(cutoff)
             , ewald(std::move(ewald_)) {
         assert(refer_cell.getNumParticle() % 3 == 0 && "[Error]: This is not a cell of SiO2");
         const size_t numMolecule = refer_cell.getNumParticle() / 3;
         ewald = REwaldType(refer_cell.getLattice(), AABModelType::makeCharges(numMolecule, chargeO, chargeSi));
+        if constexpr (AvoidTooNear) {
+            doublePot0_OO = pot_functor_impl(A_OO, b_OO, c_OO, r0_OO, r0_OO * r0_OO);
+            doublePot0_SiO = pot_functor_impl(A_SiO, b_SiO, c_SiO, r0_SiO, r0_SiO * r0_SiO);
+        }
     }
 
-    template<class ScalarType, class EwaldType>
-    ScalarType BKSModel<ScalarType, EwaldType>::pot_functor(size_t i, size_t j, ScalarType r, ScalarType r2) const {
+    template<class ScalarType, class EwaldType, bool AvoidTooNear>
+    ScalarType BKSModel<ScalarType, EwaldType, AvoidTooNear>::pot_functor(size_t i, size_t j, ScalarType r, ScalarType r2) const {
+        const size_t numMolecule = getNumMolecule();
+        if (i > j)
+            std::swap(i, j);
+        const bool isSiSi = i >= 2 * numMolecule;
+        if (isSiSi)
+            return ScalarType(0);
+        const bool isOO = j < 2 * numMolecule;
+        const double A = isOO ? A_OO : A_SiO;
+        const double b = isOO ? b_OO : b_SiO;
+        const double c = isOO ? c_OO : c_SiO;
+        const ScalarType pot = pot_functor_impl(A, b, c, r, r2);
+        if constexpr (AvoidTooNear) {
+            const double r0 = isOO ? r0_OO : r0_SiO;
+            const ScalarType doublePot0 = isOO ? doublePot0_OO : doublePot0_SiO;
+            return r < r0 ? (doublePot0 - pot) : pot;
+        }
+        else
+            return pot;
+    }
+
+    template<class ScalarType, class EwaldType, bool AvoidTooNear>
+    ScalarType BKSModel<ScalarType, EwaldType, AvoidTooNear>::force_functor(size_t i, size_t j, ScalarType r, ScalarType r2) const {
         const size_t numMolecule = getNumMolecule();
         if (i > j)
             std::swap(i, j);
@@ -126,41 +158,31 @@ namespace Physica::Core {
         const double b = isOO ? b_OO : b_SiO;
         const double c = isOO ? c_OO : c_SiO;
         const ScalarType r4 = square(r2);
-        return ScalarType(A) * exp(ScalarType(-b) * r) - ScalarType(c) / (r2 * r4);
+        const ScalarType f = ScalarType(A * b) * exp(ScalarType(-b) * r) - ScalarType(6 * c) / (r * r2 * r4);
+        if constexpr (AvoidTooNear) {
+            const double r0 = isOO ? r0_OO : r0_SiO;
+            return r < r0 ? -f : f;
+        }
+        else
+            return f;
     }
 
-    template<class ScalarType, class EwaldType>
-    ScalarType BKSModel<ScalarType, EwaldType>::force_functor(size_t i, size_t j, ScalarType r, ScalarType r2) const {
-        const size_t numMolecule = getNumMolecule();
-        if (i > j)
-            std::swap(i, j);
-        const bool isSiSi = i >= 2 * numMolecule;
-        if (isSiSi)
-            return ScalarType(0);
-        const bool isOO = j < 2 * numMolecule;
-        const double A = isOO ? A_OO : A_SiO;
-        const double b = isOO ? b_OO : b_SiO;
-        const double c = isOO ? c_OO : c_SiO;
-        const ScalarType r4 = square(r2);
-        return ScalarType(A * b) * exp(ScalarType(-b) * r) - ScalarType(6 * c) / (r * r2 * r4);
-    }
-
-    template<class ScalarType, class EwaldType>
-    inline ScalarType BKSModel<ScalarType, EwaldType>::potentialEnergy(const MDCellType& cell) const {
+    template<class ScalarType, class EwaldType, bool AvoidTooNear>
+    inline ScalarType BKSModel<ScalarType, EwaldType, AvoidTooNear>::potentialEnergy(const MDCellType& cell) const {
         return Base::potentialEnergy(cell) + ewald.potentialEnergy(cell.getPos());
     }
 
-    template<class ScalarType, class EwaldType>
+    template<class ScalarType, class EwaldType, bool AvoidTooNear>
     template<class Executor>
-    Vector<ScalarType> BKSModel<ScalarType, EwaldType>::force(const MDCellType& cell) {
+    Vector<ScalarType> BKSModel<ScalarType, EwaldType, AvoidTooNear>::force(const MDCellType& cell) {
         Vector<ScalarType> result;
         forceAsync<Vector<ScalarType>, Executor>(cell, result);
         return result;
     }
 
-    template<class ScalarType, class EwaldType>
+    template<class ScalarType, class EwaldType, bool AvoidTooNear>
     template<class VectorType, class Executor>
-    void BKSModel<ScalarType, EwaldType>::forceAsync(const MDCellType& cell, ContinuousVector<VectorType>& result) {
+    void BKSModel<ScalarType, EwaldType, AvoidTooNear>::forceAsync(const MDCellType& cell, ContinuousVector<VectorType>& result) {
         static_assert(!Internal::Traits<Executor>::isCudaEnabled, "[Error]: Cuda is not supported");
         assert(cell.getNumParticle() % 3 == 0);
         auto future = Executor::schedule([this, &cell, &result]() {
@@ -172,42 +194,53 @@ namespace Physica::Core {
         result += coulomb;
     }
 
-    template<class ScalarType, class EwaldType>
+    template<class ScalarType, class EwaldType, bool AvoidTooNear>
     template<class Executor>
-    Vector<ScalarType> BKSModel<ScalarType, EwaldType>::force_short(const MDCellType& cell) const {
+    Vector<ScalarType> BKSModel<ScalarType, EwaldType, AvoidTooNear>::force_short(const MDCellType& cell) const {
         return Base::template force_short<Executor>(cell);
     }
 
-    template<class ScalarType, class EwaldType>
+    template<class ScalarType, class EwaldType, bool AvoidTooNear>
     template<class Executor>
-    inline Vector<ScalarType> BKSModel<ScalarType, EwaldType>::force_long(const MDCellType& cell) {
+    inline Vector<ScalarType> BKSModel<ScalarType, EwaldType, AvoidTooNear>::force_long(const MDCellType& cell) {
         return ewald.template force<Executor>(cell.getPos());
     }
 
-    template<class ScalarType, class EwaldType>
-    typename BKSModel<ScalarType, EwaldType>::LatticeMatrix
-    inline BKSModel<ScalarType, EwaldType>::virial(const MDCellType& cell) {
+    template<class ScalarType, class EwaldType, bool AvoidTooNear>
+    typename BKSModel<ScalarType, EwaldType, AvoidTooNear>::LatticeMatrix
+    inline BKSModel<ScalarType, EwaldType, AvoidTooNear>::virial(const MDCellType& cell) {
         return Base::virial(cell) + ewald.virial(cell.getPos());
     }
 
-    template<class ScalarType, class EwaldType>
-    void BKSModel<ScalarType, EwaldType>::swap(BKSModel& __restrict obj) noexcept {
+    template<class ScalarType, class EwaldType, bool AvoidTooNear>
+    void BKSModel<ScalarType, EwaldType, AvoidTooNear>::swap(BKSModel& __restrict obj) noexcept {
         assert(this != &obj && "[Error]: Self swap is likely a bug");
         ewald.swap(obj.ewald);
+        if constexpr (AvoidTooNear) {
+            doublePot0_OO.swap(obj.doublePot0_OO);
+            doublePot0_SiO.swap(obj.doublePot0_SiO);
+        }
     }
 
-    template<class ScalarType, class EwaldType>
-    inline void BKSModel<ScalarType, EwaldType>::setLattice(LatticeMatrix lattice) {
+    template<class ScalarType, class EwaldType, bool AvoidTooNear>
+    inline void BKSModel<ScalarType, EwaldType, AvoidTooNear>::setLattice(LatticeMatrix lattice) {
         ewald.setLattice(std::move(lattice));
     }
 
-    template<class ScalarType, class EwaldType>
-    inline PermutationMatrix<ScalarType> BKSModel<ScalarType, EwaldType>::sortPosition(MDCellType& cell) {
+    template<class ScalarType, class EwaldType, bool AvoidTooNear>
+    inline PermutationMatrix<ScalarType> BKSModel<ScalarType, EwaldType, AvoidTooNear>::sortPosition(MDCellType& cell) {
         return AABModelType::sortPosition(cell, 8, 14);
     }
 
-    template<class ScalarType, class EwaldType>
-    inline bool BKSModel<ScalarType, EwaldType>::isCellOrdered(const MDCellType& cell) {
+    template<class ScalarType, class EwaldType, bool AvoidTooNear>
+    inline ScalarType BKSModel<ScalarType, EwaldType, AvoidTooNear>::pot_functor_impl(
+            ScalarType A, ScalarType b, ScalarType c, ScalarType r, ScalarType r2) {
+        const ScalarType r4 = square(r2);
+        return A * exp(-b * r) - c / (r2 * r4);
+    }
+
+    template<class ScalarType, class EwaldType, bool AvoidTooNear>
+    inline bool BKSModel<ScalarType, EwaldType, AvoidTooNear>::isCellOrdered(const MDCellType& cell) {
         return AABModelType::isCellOrdered(cell, 8, 14);
     }
 }
--- test/Core/Physics/MD/ForceModel/BKSModel.cpp
+++ test/Core/Physics/MD/ForceModel/BKSModel.cpp
@@ -34,7 +34,7 @@ namespace Physica {
         using LatticeMatrix = typename MDCellType::LatticeMatrix;
         using PositionMatrix = typename MDCellType::PositionMatrix;
         using MassVector = typename MDCellType::MassVector;
-        using ForceModel = BKSModel<ScalarType, Ewald<ScalarType>>;
+        using ForceModel = BKSModel<ScalarType, Ewald<ScalarType>, false>;
         constexpr static size_t MoleculePerCell = 4;
         constexpr static double pair_cutoff = PhyConst<AU>::angstormToBohr(5);
     public:
