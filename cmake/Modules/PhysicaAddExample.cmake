225a4e31a91dbfb0545ecfa2972723bd6efb0701
NewSigma <NewSigma@163.com>
2024-05-29 20:26:02 +0800

    Move Physica to src

--- cmake/Modules/PhysicaAddExample.cmake
+++ cmake/Modules/PhysicaAddExample.cmake
@@ -1,13 +1,12 @@
 file(GLOB_RECURSE HEADERS ${CMAKE_HOME_DIRECTORY}/include/*.h ${CMAKE_HOME_DIRECTORY}/include/*.cuh)
 set(PHYSICA_EXAMPLE_LIBS PhysicaCore pthread ${GPerfTools_LIBRARY})
 
-if(${PHYSICA_GUI})
-    set(CMAKE_AUTOMOC ON)
-    set(PHYSICA_EXAMPLE_LIBS ${PHYSICA_EXAMPLE_LIBS} PhysicaGui)
-endif()
-
 function(physica_add_example NAME SOURCES)
     add_executable(Example-${NAME} ${SOURCES} ${ARGN} ${HEADERS})
     target_link_libraries(Example-${NAME} ${PHYSICA_EXAMPLE_LIBS})
+    if(${PHYSICA_GUI})
+        set_target_properties(Example-${NAME} PROPERTIES AUTOMOC ON)
+        target_link_libraries(Example-${NAME} PhysicaGui)
+    endif()
     install(TARGETS Example-${NAME} DESTINATION examples)
 endfunction()
