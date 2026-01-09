57b689b14f19a8d72b3527f3912e3f7d57cf117f
NewSigma <NewSigma@163.com>
2026-01-09 20:50:52 +0800

    DenseLU: Use baseline for inverse

---
+++ benchmark/Math/MatrixDecomp/DenseLU/Inverse.cpp
@@ -0,0 +1,60 @@
+/*
+ * Copyright 2026 Weibo He.
+ *
+ * This file is part of Physica.
+ *
+ * Physica is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Physica is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Physica.  If not, see <https://www.gnu.org/licenses/>.
+ */
+#include "Physica/Core/Math/Algebra/LinearAlgebra/MatrixDecomp/DenseLU.h"
+#include "Benchmark.h"
+
+using namespace Physica;
+using RandomSource = Random<MCG>;
+
+namespace {
+    void lu(benchmark::State& state) {
+        using T = float64;
+        const size_t order = state.range(0);
+        auto m = MatrixND<T>::template random_uniform<RandomSource>(order, order);
+        auto lu = DenseLU<T, false>(m);
+        auto inv = lu.inv();
+        for (auto _ : state) {
+            inv.assign(m);
+            benchmark::DoNotOptimize(m);
+            benchmark::ClobberMemory();
+        }
+    }
+
+    void lu_base(benchmark::State& state) {
+        using T = float64;
+        const size_t order = state.range(0);
+        auto m = MatrixND<T>::template random_uniform<RandomSource>(order, order);
+        auto lu = DenseLU<T, false>(m);
+        auto inv = lu.inv();
+        for (auto _ : state) {
+            PHYSICA_BENCH(inv.assign_base(m));
+            benchmark::DoNotOptimize(m);
+            benchmark::ClobberMemory();
+        }
+    }
+}
+
+BENCHMARK(lu)->Name("DenseLU inv")
+    ->Arg(8)
+    ->Arg(64)
+    ->Arg(1024);
+BENCHMARK(lu_base)->Name("DenseLU inv base")
+    ->Arg(8)
+    ->Arg(64)
+    ->Arg(1024);
--- include/Physica/Core/Math/Algebra/LinearAlgebra/MatrixDecomp/DenseLUImpl/Inverse.h
+++ include/Physica/Core/Math/Algebra/LinearAlgebra/MatrixDecomp/DenseLUImpl/Inverse.h
@@ -58,7 +58,7 @@ namespace Physica {
     template<Scalar T, bool Pivot>
     void Inverse<DenseLU<T, Pivot>>::assign(Matrix auto& target) const {
         target.assert_assign(*this);
-        if constexpr (Internal::EnableMKL<typename LU::WorkingMatrix, decltype(target)>::value)
+        if constexpr (Internal::EnableMKL<typename LU::WorkingMatrix, decltype(target)>::value && Pivot)
             assign_mkl(target);
         else
             assign_base(target);
