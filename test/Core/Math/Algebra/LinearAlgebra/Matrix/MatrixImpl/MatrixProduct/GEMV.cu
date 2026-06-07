1d285e4aa6bc97b5affadf9dc575d3a7c88669d5
NewSigma <NewSigma@163.com>
2026-06-07 20:56:31 +0800

    Add a cuda GEMV test (NFC)

---
+++ test/Core/Math/Algebra/LinearAlgebra/Matrix/MatrixImpl/MatrixProduct/GEMV.cu
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2026 Weibo He.
+ *
+ * This file is part of Physica.
+ *
+ * Physica is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Physica is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Physica.  If not, see <https://www.gnu.org/licenses/>.
+ */
+#include "Physica/Core/Math/Algebra/LinearAlgebra/Matrix/DenseMatrix.cuh"
+#include "Test.h"
+
+using namespace Physica;
+using RandomSource = Random<>;
+
+namespace {
+    void rvalue_host_device() {
+        constexpr int N = 128;
+        using T = float32;
+        const auto A = MatrixND<T>::random_uniform<RandomSource>(N, N);
+        const auto x = VectorND<T>::random_uniform<RandomSource>(N);
+        VectorND<T> y = A * exp(x);
+        auto dy = device_obj<VectorND<T>>(A.toDevice() * exp(x.toDevice())).toHost();
+        expect<RandomSource>(vectorNear(y, dy, 1E-6));
+
+        y = A.transpose() * exp(x);
+        dy = device_obj<VectorND<T>>(A.toDevice().transpose() * exp(x.toDevice())).toHost();
+        expect<RandomSource>(vectorNear(y, dy, 1E-6));
+    }
+}
+
+int main() {
+    rvalue_host_device();
+    return 0;
+}
